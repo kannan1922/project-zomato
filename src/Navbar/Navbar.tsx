17ef1fff1c85f5be123385fd17f3b52873254125
kannan <kannan.d@codingmart.com>
2023-12-11 11:43:08 +0530

    change App file

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -667,7 +667,7 @@ useEffect(() => {
       {showSignUp&&
       <div className='overlay1' >
         <div className='signUp'>
-         <div className='signupLogo'> Sign</div>
+         <div className='signupLogo'> Sign Up</div>
          <div className='exitSignup' onClick={handleExit}> <svg xmlns="http://www.w3.org/2000/svg" fill="#1C1C1C" width="24" height="24" viewBox="0 0 20 20" aria-labelledby="icon-svg-title- icon-svg-desc-" role="img" ><title>cross</title><path d="M11.42 10.42l3.54-3.54c0.38-0.4 0.38-1.040 0-1.42s-1.020-0.4-1.42 0l-3.54 3.54-3.54-3.54c-0.4-0.4-1.020-0.4-1.42 0s-0.38 1.020 0 1.42l3.54 3.54-3.54 3.54c-0.38 0.38-0.38 1.020 0 1.42 0.2 0.18 0.46 0.28 0.72 0.28s0.5-0.1 0.7-0.28l3.54-3.56 3.54 3.56c0.2 0.18 0.46 0.28 0.72 0.28s0.5-0.1 0.7-0.28c0.38-0.4 0.38-1.040 0-1.42l-3.54-3.54z"></path></svg></div>
           </div>
           <div className="inputContainer">
